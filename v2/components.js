c1b15d251e64bfb23dccaebb57b452d5f5eb7dd3
Cedric Quenette <c@cedricq.me>
2020-11-21 11:32:58 +1100

    Refactored components to utilise ui element builder

--- v2/components.js
+++ v2/components.js
@@ -1,12 +1,9 @@
-attributeComponent = (attribute, index) => {
+attributeComponent = (attribute) => {
   let attributeDiv = document.createElement("DIV");
 
-  let attributeNameElement = document.createElement("P");
-  let attributeNameText = document.createTextNode(attribute);
-  attributeNameElement.appendChild(attributeNameText);
+  let attributeNameElement = createParagraph("none", "none", attribute);
 
-  let attributeValue = document.createElement("INPUT");
-  attributeValue.type = "text";
+  let attributeValue = createTextInput("none", "none");
 
   attributeDiv.appendChild(attributeNameElement);
   attributeDiv.appendChild(attributeValue);
@@ -18,66 +15,48 @@ function characteristicComponent(characteristic) {
   let characteristicDiv = document.createElement("DIV");
   characteristicDiv.classList.add("characteristic-div");
 
-  let characteristicCheckbox = document.createElement("INPUT");
-  characteristicCheckbox.type = "checkbox";
-  characteristicCheckbox.classList.add("characteristic-checkbox");
-  characteristicCheckbox.id = characteristic + "Checkbox";
+  let characteristicCheckbox = createCheckbox("characteristic-checkbox", characteristic + "Checkbox");
 
-  let characteristicNameElement = document.createElement("P");
-  let characteristicNameText = document.createTextNode(characteristic);
-  characteristicNameElement.appendChild(characteristicNameText);
+  let characteristicNameElement = createParagraph("none", "none", characteristic);
 
   let characteristicValueDiv = document.createElement("DIV");
   characteristicValueDiv.classList.add("characteristic-value-div");
 
-  let characteristicRegularValue = document.createElement("INPUT");
-  characteristicRegularValue.type = "text";
-  characteristicRegularValue.id = characteristic + "ValueInput";
+  let characteristicRegularValue = createTextInput("none", characteristic + "ValueInput");
   characteristicRegularValue.onchange = function () {
     calculateSuccessCharacteristicValues(characteristic);
   };
 
-  let characteristicHardValue = document.createElement("P");
-  characteristicHardValue.classList.add("-hard-value-paragraph");
-  characteristicHardValue.id = characteristic + "-hard-value-paragraph";
+  let characteristicHardValue = createParagraph("-hard-value-paragraph", characteristic + "-hard-value-paragraph", "none");
 
-  let characteristicExtremeValue = document.createElement("P");
-  characteristicExtremeValue.classList.add("-extreme-value-parahraph");
-  characteristicExtremeValue.id = characteristic + "-extreme-value-paragraph";
+  let characteristicExtremeValue = createParagraph("-extreme-value-paragraph", characteristic + "-extreme-value-paragraph", "none");
 
   characteristicValueDiv.appendChild(characteristicRegularValue);
   characteristicValueDiv.appendChild(characteristicHardValue);
   characteristicValueDiv.appendChild(characteristicExtremeValue);
 
-  let rollButton = document.createElement("BUTTON");
-  rollButton.classList.add("roll-button");
+  let rollButton = createButton("roll-button", "none", "Roll");
   rollButton.onclick = function () {
     roller(rollButton);
   };
-  rollButton.innerHTML = "Roll";
 
   let useLuckDiv = document.createElement("DIV");
   useLuckDiv.classList.add("luck-div");
 
-  let useLuckInput = document.createElement("INPUT");
-  useLuckInput.type = "text";
-  useLuckInput.id = characteristic + "luckInput";
+  let useLuckInput = createTextInput("none", characteristic + "luckInput");
   useLuckInput.onchange = function () {
     useLuck(characteristic);
   };
 
-  let useLuckButton = document.createElement("BUTTON");
-  useLuckButton.innerHTML = "Use Luck";
+  let useLuckButton = createButton("none", "none", "Use Luck")
 
   useLuckDiv.appendChild(useLuckInput);
   useLuckDiv.appendChild(useLuckButton);
 
-  let pushButton = document.createElement("BUTTON");
-  pushButton.classList.add("push-button");
+  let pushButton = createButton("push-button", "none", "Push");
   pushButton.onclick = function () {
     push();
   };
-  pushButton.innerHTML = "Push";
 
   characteristicDiv.appendChild(characteristicCheckbox);
   characteristicDiv.appendChild(characteristicNameElement);
--- v2/ui.js
+++ v2/ui.js
@@ -1,33 +1,66 @@
-const checkboxElement = (className, id) => {
-  let checkboxElement = document.createElement('input');
-  checkboxElement.type = 'checkbox';
-  checkboxElement.classList.add(className);
-  checkboxElement.id = id;
+const createCheckbox = (className, id) => {
+  let checkboxElement = document.createElement("input");
+  checkboxElement.type = "checkbox";
+
+  if (className != "none") {
+    checkboxElement.classList.add(className);
+  }
+
+  if (id != "none") {
+    checkboxElement.id = id;
+  }
 
   return checkboxElement;
-}
+};
 
-const paragraphElement = (className, id) => {
-  let paragraphElement = document.createElement('p');
-  paragraphElement.classList.add(className);
-  paragraphElement.id = id;
+const createParagraph = (className, id, content) => {
+  let paragraphElement = document.createElement("p");
+
+  if (className != "none") {
+    paragraphElement.classList.add(className);
+  }
+
+  if (id != "none") {
+    paragraphElement.id = id;
+  }
+
+  if (content != "none") {
+    let textNode = document.createTextNode(content);
+    paragraphElement.appendChild(textNode);
+  }
 
   return paragraphElement;
-}
+};
 
-const textInputElement = (className, id) => {
-  let textInput = document.createElement('input');
+const createTextInput = (className, id) => {
+  let textInput = document.createElement("input");
   textInput.type = "text";
-  textInput.classList.add(className);
-  textInput.id = id;
+
+  if (className != "none") {
+    textInput.classList.add(className);
+  }
+
+  if (id != "none") {
+    textInput.id = id;
+  }
 
   return textInput;
-}
+};
 
-const buttonElement = (className, id) => {
-  let buttonElement = document.createElement('button');
-  buttonElement.classList.add(className);
-  buttonElement.id = id;
+const createButton = (className, id, text) => {
+  let buttonElement = document.createElement("button");
+
+  if (className != "none") {
+    buttonElement.classList.add(className);
+  }
+
+  if (id != "none") {
+    buttonElement.id = id;
+  }
+
+  if (text != "none") {
+    buttonElement.innerHTML = text;
+  }
 
   return buttonElement;
-}
+};
